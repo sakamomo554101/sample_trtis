9ca90a61c65e985459b0548b5d5a120070b1b31c
kurama554101 <shotasakamoto554101@gmail.com>
2020-02-21 15:45:26 +0900

    [Mod] enable to setup the server and client environments separately

--- setup_custom_backend.sh
+++ setup_custom_backend.sh
@@ -19,15 +19,7 @@ mkdir -p model_repository/face_recognition_model/1
 cp -f ./model_settings/face_recognition_model/config.pbtxt \
       ./model_repository/face_recognition_model/
 
-# TODO : build the server codes
-echo "----------  start to build trtis-server-container  ------------"
-
-# build the client codes
-echo "----------  start to build trtis-client-container  ------------"
-docker exec -it trtis-client-container sh -c "cd /workspace && bash build_client_library.sh"
-
 # build the custom-backend codes
-echo "----------  start to build trtis-custom-backend-build-container  ------------"
 ## add the build target source into CMakeLists.txt(TODO : write target into CMakeLists.txt if needed.)
 docker exec -it $CUSTOM_BACKEND_CONTAINER_NAME sh -c \
     "echo 'add_subdirectory(../../custom_backend/sample_instance src/custom/sample_instance)' >> /workspace/build/trtis-custom-backends/CMakeLists.txt"
@@ -37,5 +29,19 @@ docker exec -it $CUSTOM_BACKEND_CONTAINER_NAME sh -c \
     "echo 'add_subdirectory(../../custom_backend/mecab_model src/custom/mecab_model)' >> /workspace/build/trtis-custom-backends/CMakeLists.txt"
 docker exec -it $CUSTOM_BACKEND_CONTAINER_NAME sh -c \
     "echo 'add_subdirectory(../../custom_backend/face_recognition_model src/custom/face_recognition_model)' >> /workspace/build/trtis-custom-backends/CMakeLists.txt"
-## build custom backend files and move libraries
-bash build_custom_models.sh
+
+## build custom backend files
+docker exec -it $CUSTOM_BACKEND_CONTAINER_NAME sh -c "cd /workspace && bash build_custom_backend.sh"
+
+# move so object from custom_backend_container
+CONTAINER_ID=$(docker ps -a -f name=$CUSTOM_BACKEND_CONTAINER_NAME --format "{{.ID}}")
+docker cp $CONTAINER_ID:/workspace/build/trtis-custom-backends/src/custom/sample_instance/libsample_instance.so \
+          model_repository/sample_instance/1/
+docker cp $CONTAINER_ID:/workspace/build/trtis-custom-backends/src/custom/sequence/libsequence.so \
+          model_repository/simple_sequence/1/
+docker cp $CONTAINER_ID:/workspace/build/trtis-custom-backends/src/custom/sample_sequence/libsample_sequence.so \
+          model_repository/sample_sequence/1/
+docker cp $CONTAINER_ID:/workspace/build/trtis-custom-backends/src/custom/mecab_model/libmecab_model.so \
+          model_repository/mecab_model/1/
+docker cp $CONTAINER_ID:/workspace/build/trtis-custom-backends/src/custom/face_recognition_model/libface_recognition_model.so \
+          model_repository/face_recognition_model/1/
